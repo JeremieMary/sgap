5d91c5420525a4332d4cd0264211c921950a9e93
Jérémie <jeremie@Babul.local>
2013-10-17 22:20:16 +0200

    minor fixes

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -6,23 +6,20 @@
 <?php echo form_open_multipart('admin/uploadFile/users', array('name'=>'users') );?>
 <label for="userfile"> Chargement des utilisateurs - CSV : </label>
 <input type="file" name="usersfile" size="20" />
-<input type="submit" value="upload" />	
-</div>
+<input type="submit"  value="upload" />
+</form>	</div>
 
 <div class="upload_form">
 <?php echo form_open_multipart('admin/uploadFile/cycles', array('name'=>'cycles') );?>
-<label for="userfile"> Chargement des  cycles - CSV : </label>
+<label for="cyclesfile"> Chargement des cycles - CSV : </label>
 <input type="file" name="cyclesfile" size="20" />
 <input type="submit" value="upload" />	
-</div>
+</form>	</div>
 
 <div class="upload_form">
 <?php echo form_open_multipart('admin/uploadFile/matieres', array('name'=>'matieres') );?>
-<label for="userfile"> Chargement des matières - CSV: </label>
+<label for="matieresfile"> Chargement des matières - CSV: </label>
 <input type="file" name="matieresfile" size="20" />
 <input type="submit" value="upload" />	
-</div>
+</form>	</div>
 
-
-
-</form>
