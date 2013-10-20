ead295e40ce53be58add104217c632f097a4e91c
Jérémie <jeremie.mary@inria.fr>
2013-10-20 23:56:25 +0200

    cycles_model

---
+++ application/models/cycles_model.php
@@ -0,0 +1,70 @@
+<?
+class Cycles_model extends CI_Model {
+	
+	public function __construct()
+	{
+		$this->load->database();
+	}
+	
+	function commitCycle( $cycle )
+	{
+		//Ajouter des vérifications de cohérence ? 
+		$cycle['actif']=1;
+		$dup ='  ON DUPLICATE KEY UPDATE';
+		$fieldsToUpdate = array('dates','actif' );
+		foreach ( $fieldsToUpdate as $champ ) $dup .= " `$champ`=VALUES(`$champ`)," ;
+		$dup = rtrim($dup, ",");
+		$dup .= ';';
+		$sql = $this->db->insert_string('cycles', $cycle) . $dup;
+		$this->db->query($sql);
+	}
+		
+	
+	private function formatForDB($cycle) {
+		return ( array( 'debut'=>reset($cycle), 'dates'=>serialize(array_values($cycle) ) ) );
+	}
+	
+	function commitArray($tab)
+	{
+		//Inactive all users not in array. It will temporarly break everything
+		$this->db->update('cycles', array('actif'=>0) ); 
+		foreach ($tab as $cycle ){
+			$this->commitCycle( $this->formatForDB($cycle) );
+		}
+	}	
+		
+	function check( $tab )
+	{
+		//Quick and dirty. I just assume it's not used often nor for big files
+		$data['ajouts']=array();
+		$data['modifications']=array();
+		$data['desactive']=array();	
+		$debuts = array();
+		foreach ($tab as $row ){
+			array_push( $debuts, reset($row) );
+		}
+			
+		$this -> db -> select('debut,dates');
+		$this -> db -> from('cycles');
+		$query = $this -> db -> get();
+		$allcycles= $query->result_array();
+		$dbdebuts = array();
+		foreach( $allcycles as $cycle ){
+			$first = reset($cycle);
+			array_push( $dbdebuts, $first );
+			if ( ! in_array( $first, $debuts ) ) {
+				array_push( $data['desactive'], $cycle );
+			}
+		}
+		foreach( $tab as $cycle ){
+			$first = reset($cycle);
+			if (  in_array( $first, $dbdebuts ) ) { 
+				array_push( $data['modifications'], $cycle );
+			} else {
+				array_push( $data['ajouts'], $cycle );
+			}
+		}
+		return($data);
+	}
+	
+}
