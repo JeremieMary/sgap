a9fd3b38ae0550823732622b2acd6dfbca653a5f
Jérémie <jeremie.mary@inria.fr>
2013-10-23 12:42:29 +0200

    typo

--- application/models/accompagnement_model.php
+++ application/models/accompagnement_model.php
@@ -12,18 +12,14 @@ class Accompagnement_model extends CI_Model {
 	}
 	
 	function getId($cycle_id, $matiere_id){
-		echo $cycle_id;
-		echo $matiere_id;
-		echo 'foo';
 		$accompagnement = array('matiere_id'=>$matiere_id,'cycle_id'=>$cycle_id );
-		$query = $this->db->get_where('accompagnement_id',$accompagnement,1);
+		$query = $this->db->get_where('accompagnement',$accompagnement,1);
 		if ($query->num_rows() == 1 ) { 
 			$res = $query->row_array();
 			return( $res['id'] ) ;
 		} else {
-			$sql = $this->db->insert_string('accompagnement', $accompagnement);
-			$this->db->query($sql);
-			return( $this->db->insert_id();)
+			$sql = $this->db->insert('accompagnement', $accompagnement);
+			return( $this->db->insert_id() );
 		}
 	}
 		
