307acfe9166052a6e01c4f4d026bc3e3c6acc205
Jérémie <jeremie.mary@inria.fr>
2013-11-12 17:01:53 +0100

    update vues éleves

--- application/views/eleve/js.php
+++ application/views/eleve/js.php
@@ -1,29 +1,3 @@
-<h2>Inscriptions</h2>
-<div class="cycles">
-<ul>
-	<? foreach ($cycles as $cycle){?>
-		<li name='<?=$cycle["id"]?>'> <?
-			$date =date_create_from_format("d/m/Y",$cycle['debut']);
-			$timestamp = $date->getTimestamp(); 
-			echo strftime( "%a %d/%m/%Y", $timestamp ); ?>
-	<?}?>
-</ul>
-</div>
-
-<div class="matieres">
-<ul>
-	<? foreach ($matieres as $matiere){?>
-		<li name='<?=$matiere["id"]?>'> <?=$matiere['nom']?>
-	<?}?>
-</ul>
-</div>
-
-<div class="infos">
-<ul>
-<li> Nombre de places : <span id='nbPlaces'></span> </li>
-<li> Nombre d'inscrits : <span id='nbInscrits'></span> </li>
-<li> Dates : <span id='dates'></span> </li>
-
 <script type='text/javascript'>
 var accompagnement=<?=json_encode( $accompagnement )?>	
 
