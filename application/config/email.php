30d56172cfa0109d00298c19d4141f4706c2958b
utilisateur autres <alice.nowicki@ms001-11.pedapdb.local>
2014-01-15 10:45:55 +0100

    email.php ajoute

---
+++ application/config/email.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Configuration EMAIL
+| -------------------------------------------------------------------------
+*/
+ 
+$config['protocol']        = 'smtp';
+$config['smtp_host']    = 'smtp.free.fr';
+$config['smtp_port']    = 25;
+$config['mailtype']        = 'html';
+$config['charset']        = 'utf-8';
+$config['wordwrap'] = TRUE;
+
