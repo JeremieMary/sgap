1bb48820ff42fced0f0f97f897c8c1cf0d4df854
Jérémie <jeremie.mary@inria.fr>
2014-01-26 23:14:31 +0100

    css fix

--- application/views/user/changepasswd.php
+++ application/views/user/changepasswd.php
@@ -1,12 +1,14 @@
 <div class="container">
 		<div class="form-bg">
+			<div id="login">
 			<?php echo form_open('user/changePasswd'); ?>		
 				<h2>Changement de mot de passe</h2>
 				<p><input type="password" placeholder="Ancien Password" name="opasswd"  /></p>
 				<p><input type="password" placeholder="Nouveau Password" name="npasswd"  /></p>
 				<p><input type="password" placeholder="Nouveau Password" name="npasswd"  /></p>
-				<button type="submit" class="confirm"></button>
+				<center><button type="submit" class="confirm">Envoi</button></center>
 			</form>
+		</div>
 		</div>
 	</div>
 </div>
--- application/views/user/reset.php
+++ application/views/user/reset.php
@@ -1,9 +1,9 @@
 <div class="container">
-		<div class="form-bg">
+		<div class="form-bg"><div id='login'>
 			<?php echo form_open('user/reset'); ?>
 				<h2>Entrez votre mail</h2>
 				<p><input type="text" placeholder="foo@bar.com" name="mail" value="<?= set_value('mail'); ?>" /></p>
 				<button type="submit">Envoyer</button>
 			</form>
-		</div>
+		</div></div>
 </div>
