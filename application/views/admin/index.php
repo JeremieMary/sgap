02cf2801599f6a621ef54c6a36a3e9c8e7c740ed
Jérémie <jeremie.mary@inria.fr>
2014-02-07 11:07:42 +0100

    upload supprimé

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -11,21 +11,21 @@
 <?php echo form_open_multipart('admin/uploadFile/users', array('name'=>'users') );?>
 <label for="userfile"> Chargement des utilisateurs - CSV : </label>
 <input type="file" name="usersfile" size="20" onchange="this.form.submit();" />
-<input type="submit"  value="Upload" />
+<!-- <input type="submit"  value="Upload" /> -->
 </form>	</div>
 
 <div class="upload_form">
 <?php echo form_open_multipart('admin/uploadFile/cycles', array('name'=>'cycles') );?>
 <label for="cyclesfile"> Chargement des cycles - CSV : </label>
 <input type="file" name="cyclesfile" size="20" onchange="this.form.submit();" />
-<input type="submit" value="Upload" />	
+<!-- <input type="submit" value="Upload" />	-->
 </form>	</div>
 
 <div class="upload_form">
 <?php echo form_open_multipart('admin/uploadFile/matieres', array('name'=>'matieres') );?>
 <label for="matieresfile"> Chargement des matières - CSV : </label>
 <input type="file" name="matieresfile" size="20" onchange="this.form.submit();" />
-<input type="submit" value="Upload" />	
+<!--  <input type="submit" value="Upload" /> -->	
 </form>	</div>
 
 <div class="upload_form">
